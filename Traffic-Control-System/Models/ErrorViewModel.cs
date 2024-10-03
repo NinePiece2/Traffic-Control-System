de06e501f2d0eb098c18d11edb08fdc4a4048803
NinePiece2 <romit.sagu@hotmail.com>
2024-10-02 21:09:51 -0400

    Add project files.

---
+++ Traffic-Control-System/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Traffic_Control_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
