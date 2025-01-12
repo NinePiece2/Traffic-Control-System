1729af2fd0c153af1bc61af3d8e5680abecedebb
NinePiece2 <romit.sagu@hotmail.com>
2025-01-12 16:52:05 -0500

    Update URLs add add ClipController

---
+++ Traffic-Control-System-Video/Controllers/ClipController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+
+namespace Traffic_Control_System_Video.Controllers
+{
+    [Authorize]
+    [Route("/[controller]")]
+    [ApiController]
+    public class ClipController : ControllerBase
+    {
+    }
+}
