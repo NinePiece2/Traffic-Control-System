381fc16a85e94137768b304254a8edc7a87ba705
NinePiece2 <romit.sagu@hotmail.com>
2025-01-12 19:06:39 -0500

    Add Video Player demo

--- Traffic-Control-System/Controllers/HomeController.cs
+++ Traffic-Control-System/Controllers/HomeController.cs
@@ -5,21 +5,28 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using System.Security.Claims;
 using Traffic_Control_System.Services;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Net;
 
 namespace Traffic_Control_System.Controllers
 {
     [Authorize]
     public class HomeController : Controller
     {
+        private readonly IConfiguration config;
         private readonly ILogger<HomeController> _logger;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly IEmailService emailService;
 
-        public HomeController(ILogger<HomeController> logger, UserManager<ApplicationUser> userManager, IEmailService _emailService)
+        public HomeController(ILogger<HomeController> logger, UserManager<ApplicationUser> userManager, IEmailService _emailService, IConfiguration _config)
         {
             _logger = logger;
             _userManager = userManager;
             emailService = _emailService;
+            config = _config;
         }
 
         public async Task<IActionResult> Index()
@@ -37,9 +44,32 @@ namespace Traffic_Control_System.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public async Task<IActionResult> Video()
         {
-            return View();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(config["VideoServiceURL"]);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var apiKey = WebUtility.UrlEncode(config["API_KEY"]);
+                var param = $"userId={apiKey}";
+
+                HttpResponseMessage response = await client.GetAsync($"Token/GetToken?{param}");
+
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = response.Content.ReadAsStringAsync().Result;
+                    var token = JsonConvert.DeserializeObject<string>(responseContent);
+                    ViewBag.Token = token;
+                }
+                else
+                {
+                    ViewBag.Token = null;
+                }
+            }
+                return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
